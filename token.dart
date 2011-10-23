2bd1aa27218988d4d2d4118ba0c86dda8aa96fd8
Olov Lassus <olov.lassus@gmail.com>
2011-10-23 18:13:42 +0200

    token: formatting, private fields

--- token.dart
+++ token.dart
@@ -12,141 +12,63 @@
  */
 
 class Token {
-  static HashMap<String, Token> tokens;
-
+  /* End-of-stream. */
   static Token EOS;
 
   /* Punctuators. */
-  static Token LPAREN;
-  static Token RPAREN;
-  static Token LBRACK;
-  static Token RBRACK;
-  static Token LBRACE;
-  static Token RBRACE;
-  static Token COLON;
-  static Token SEMICOLON;
-  static Token PERIOD;
-  static Token ELLIPSIS;
-  static Token COMMA;
-  static Token CONDITIONAL;
-  static Token ARROW;
+  static Token LPAREN, RPAREN, LBRACK, RBRACK, LBRACE, RBRACE, COLON,
+    SEMICOLON, PERIOD, ELLIPSIS, COMMA, CONDITIONAL, ARROW;
 
   /* Assignment operators. */
-  static Token ASSIGN;
-  static Token ASSIGN_BIT_OR;
-  static Token ASSIGN_BIT_XOR;
-  static Token ASSIGN_BIT_AND;
-  static Token ASSIGN_SHL;
-  static Token ASSIGN_SAR;
-  static Token ASSIGN_SHR;
-  static Token ASSIGN_ADD;
-  static Token ASSIGN_SUB;
-  static Token ASSIGN_MUL;
-  static Token ASSIGN_DIV;
-  static Token ASSIGN_MOD;
-  static Token ASSIGN_TRUNC;
+  static Token ASSIGN, ASSIGN_BIT_OR, ASSIGN_BIT_XOR, ASSIGN_BIT_AND,
+    ASSIGN_SHL, ASSIGN_SAR, ASSIGN_SHR, ASSIGN_ADD, ASSIGN_SUB, ASSIGN_MUL,
+    ASSIGN_DIV, ASSIGN_MOD, ASSIGN_TRUNC;
 
   /* Binary operators sorted by precedence. */
-  static Token OR;
-  static Token AND;
-  static Token BIT_OR;
-  static Token BIT_XOR;
-  static Token BIT_AND;
-  static Token SHL;
-  static Token SAR;
-  static Token SHR;
-  static Token ADD;
-  static Token SUB;
-  static Token MUL;
-  static Token DIV;
-  static Token TRUNC;
-  static Token MOD;
+  static Token OR, AND, BIT_OR, BIT_XOR, BIT_AND, SHL, SAR, SHR, ADD, SUB,
+    MUL, DIV, TRUNC, MOD;
 
   /* Compare operators sorted by precedence. */
-  static Token EQ;
-  static Token NE;
-  static Token EQ_STRICT;
-  static Token NE_STRICT;
-  static Token LT;
-  static Token GT;
-  static Token LTE;
-  static Token GTE;
-  static Token IS;
+  static Token EQ, NE, EQ_STRICT, NE_STRICT, LT, GT, LTE, GTE, IS;
 
   /* Unary operators. */
-  static Token NOT;
-  static Token BIT_NOT;
+  static Token NOT, BIT_NOT;
 
   /* Count operators (also unary). */
-  static Token INC;
-  static Token DEC;
+  static Token INC, DEC;
 
   /* [] operator overloading. */
-  static Token INDEX;
-  static Token ASSIGN_INDEX;
+  static Token INDEX, ASSIGN_INDEX;
 
   /* Keywords. */
-  static Token BREAK;
-  static Token CASE;
-  static Token CATCH;
-  static Token CONST;
-  static Token CONTINUE;
-  static Token DEFAULT;
-  static Token DO;
-  static Token ELSE;
-  static Token FINAL;
-  static Token FINALLY;
-  static Token FOR;
-  static Token IF;
-  static Token IN;
-  static Token NEW;
-  static Token RETURN;
-  static Token SUPER;
-  static Token SWITCH;
-  static Token THIS;
-  static Token THROW;
-  static Token TRY;
-  static Token VAR;
-  static Token VOID;
-  static Token WHILE;
+  static Token BREAK, CASE, CATCH, CONST, CONTINUE, DEFAULT, DO, ELSE, FINAL,
+    FINALLY, FOR, IF, IN, NEW, RETURN, SUPER, SWITCH, THIS, THROW, TRY, VAR,
+    VOID, WHILE;
 
   /* Literals. */
-  static Token NULL_LITERAL;
-  static Token TRUE_LITERAL;
-  static Token FALSE_LITERAL;
-  static Token HEX_LITERAL;
-  static Token INTEGER_LITERAL;
-  static Token DOUBLE_LITERAL;
-  static Token STRING;
+  static Token NULL_LITERAL, TRUE_LITERAL, FALSE_LITERAL, HEX_LITERAL,
+    INTEGER_LITERAL, DOUBLE_LITERAL, STRING;
 
   /** String interpolation and string templates. */
-  static Token STRING_SEGMENT;
-  static Token STRING_LAST_SEGMENT;
+  static Token STRING_SEGMENT, STRING_LAST_SEGMENT;
   // STRING_EMBED_EXP_START does not have a unique string representation in the code:
   //   "$id" yields the token STRING_EMBED_EXP_START after the '$', and similarly
   //   "${id}" yield the same token for '${'.
-  static Token STRING_EMBED_EXP_START;
-  static Token STRING_EMBED_EXP_END;
+  static Token STRING_EMBED_EXP_START, STRING_EMBED_EXP_END;
 
   // Note: STRING_EMBED_EXP_END uses the same symbol as RBRACE, but it is
   // recognized by the scanner when closing embedded expressions in string
   // interpolation and string templates.
   
   /* Directives */
-  static Token LIBRARY;
-  static Token IMPORT;
-  static Token SOURCE;
-  static Token RESOURCE;
-  static Token NATIVE;
+  static Token LIBRARY, IMPORT, SOURCE, RESOURCE, NATIVE;
 
   /* Identifiers (not keywords). */
-  static Token IDENTIFIER;
-  static Token WHITESPACE;
+  static Token IDENTIFIER, WHITESPACE;
 
   /* Pseudo tokens. */
   // If you add another pseudo token, don't forget to update the predicate below.
-  static Token ILLEGAL;
-  static Token COMMENT;
+  static Token ILLEGAL, COMMENT;
 
   /**
    * Non-token to be used by tools where a value outside the range of anything
@@ -158,8 +80,7 @@ class Token {
    */
   static Token NON_TOKEN;
 
-
-
+  static HashMap<String, Token> tokens;
 
   /**
    * Given a string finds the corresponding token. Pseudo tokens (EOS, ILLEGAL and COMMENT) are
@@ -173,10 +94,10 @@ class Token {
     return token;
   }
 
-  final String syntax_;
-  final int precedence_;
-  final int ordinal_;
-  final String name_;
+  final String _syntax;
+  final int _precedence;
+  final int _ordinal;
+  final String _name;
 
   static initialize() {
     tokens = new HashMap<String, Token>();
@@ -294,16 +215,9 @@ class Token {
     /** String interpolation and string templates. */
     STRING_SEGMENT = T(null, 0, "STRING_SEGMENT");
     STRING_LAST_SEGMENT = T(null, 0, "STRING_LAST_SEGMENT");
-    // STRING_EMBED_EXP_START does not have a unique string representation in the code:
-    //   "$id" yields the token STRING_EMBED_EXP_START after the '$', and similarly
-    //   "${id}" yield the same token for '${'.
     STRING_EMBED_EXP_START = T(null, 0, "STRING_EMBED_EXP_START");
     STRING_EMBED_EXP_END = T(null, 0, "STRING_EMBED_EXP_END");
 
-    // Note: STRING_EMBED_EXP_END uses the same symbol as RBRACE, but it is
-    // recognized by the scanner when closing embedded expressions in string
-    // interpolation and string templates.
-  
     /* Directives */
     LIBRARY = T("#library", 0);
     IMPORT = T("#import", 0);
@@ -316,18 +230,10 @@ class Token {
     WHITESPACE = T(null, 0, "WHITESPACE");
 
     /* Pseudo tokens. */
-    // If you add another pseudo token, don't forget to update the predicate below.
     ILLEGAL = T(null, 0, "ILLEGAL");
     COMMENT = T(null, 0, "COMMENT");
 
-    /**
-     * Non-token to be used by tools where a value outside the range of anything
-     * returned by the scanner is needed. This is the equivalent of -1 in a C
-     * tokenizer.
-     *
-     * This token is never returned by the scanner. It must have an ordinal
-     * value outside the range of all tokens returned by the scanner.
-     */
+    /* Non-token */
     NON_TOKEN = T(null, 0, "NON_TOKEN");
   }
 
@@ -339,64 +245,55 @@ class Token {
    * representation, or it is a pseudo token (which doesn't physically appear
    * in the source).
    */
-  Token(syntax, precedence, ordinal, name) :
-  syntax_ = syntax, precedence_ = precedence, ordinal_ = ordinal, name_ = name {
+  Token(syntax, precedence, ordinal, name)
+      : _syntax = syntax, _precedence = precedence, _ordinal = ordinal, _name = name {
     if (null != syntax) {
       tokens[syntax] = this;
     }
   }
 
   Token asBinaryOperator() {
-    int ordinal = ordinal() - ASSIGN_BIT_OR.ordinal() + BIT_OR.ordinal();
+    int ordinal = _ordinal - ASSIGN_BIT_OR._ordinal + BIT_OR._ordinal;
     return values()[ordinal];
   }
 
-  int ordinal() => ordinal_; // TODO remove
+  int getPrecedence() => _precedence;
 
-  int getPrecedence() => precedence_; // TODO remove
-
-  String getSyntax() => syntax_; // TODO remove
+  String getSyntax() => _syntax;
 
   bool isEqualityOperator() {
-    int ordinal = ordinal();
-    return EQ.ordinal() <= ordinal && ordinal <= NE_STRICT.ordinal();
+    return EQ._ordinal <= _ordinal && _ordinal <= NE_STRICT._ordinal;
   }
 
   bool isRelationalOperator() {
-    int ordinal = ordinal();
-    return LT.ordinal() <= ordinal && ordinal <= GTE.ordinal();
+    return LT._ordinal <= _ordinal && _ordinal <= GTE._ordinal;
   }
 
   bool isAssignmentOperator() {
-    int ordinal = ordinal();
-    return ASSIGN.ordinal() <= ordinal && ordinal <= ASSIGN_TRUNC.ordinal();
+    return ASSIGN._ordinal <= _ordinal && _ordinal <= ASSIGN_TRUNC._ordinal;
   }
 
   bool isBinaryOperator() {
-    int ordinal = ordinal();
-    return (ASSIGN.ordinal() <= ordinal && ordinal <= IS.ordinal())
-        || (ordinal == COMMA.ordinal());
+    return (ASSIGN._ordinal <= _ordinal && _ordinal <= IS._ordinal)
+        || (_ordinal == COMMA._ordinal);
   }
 
   bool isCountOperator() {
-    int ordinal = ordinal();
-    return INC.ordinal() <= ordinal && ordinal <= DEC.ordinal();
+    return INC._ordinal <= _ordinal && _ordinal <= DEC._ordinal;
   }
 
   bool isUnaryOperator() {
-    int ordinal = ordinal();
-    return NOT.ordinal() <= ordinal && ordinal <= DEC.ordinal();
+    return NOT._ordinal <= _ordinal && _ordinal <= DEC._ordinal;
   }
 
   bool isUserDefinableOperator() {
-    int ordinal = ordinal();
-    return ((BIT_OR.ordinal() <= ordinal && ordinal <= GTE.ordinal())
+    return ((BIT_OR._ordinal <= _ordinal && _ordinal <= GTE._ordinal)
         || this == BIT_NOT || this == INDEX || this == ASSIGN_INDEX)
         && this != NE && this != EQ_STRICT && this != NE_STRICT;
   }
 
   String toString() {
-    assert(syntax_ != null || name_ != null);
-    return (syntax_ != null) ? syntax_ : name_;
+    assert(_syntax != null || _name != null);
+    return (_syntax != null) ? _syntax : _name;
   }
 }
